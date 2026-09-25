a5eba11a49bbef4899bdbe937ae2e8cb55a7df08
Albin Kempe <albin.kempe@outlook.com>
2026-09-25 18:29:05 +0200

    prettier

--- web/script.js
+++ web/script.js
@@ -1,4 +1,6 @@
-const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+const isLocal =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1";
 const basePath = isLocal ? "../" : "";
 
 const CONFIG = {
